204e87a9d8b57ddba1a98eecb0773e79dcd793c3
vanyakryzhanovskyi <vanyakrizhik@gmail.com>
2022-12-03 19:06:52 +0100

    task01

---
+++ task01/js/task01.js
@@ -0,0 +1,72 @@
+// ---------------text value---------------
+
+// let FirstVar = "John";
+// let SecondVar = "Trivolta";
+// document.write("User name:",FirstVar + "<br/> " + "User last name:",SecondVar );
+
+// let FirstName = "Bilbo";
+// let LastName = "Sumkin";
+// document.write("<br/>");
+// document.write("User name:",FirstName);
+// document.write("<br/>");
+// document.write("User last name:",LastName);
+
+// let FirstVar01 = prompt("User name");
+// let SecondVar01 = prompt("User last name");
+// document.write("<br/>" + "User name:",FirstVar01 + "<br/>" + "User last name:",SecondVar01);
+
+// ------------------Numeric value----------------
+
+// let UserOld = prompt("How old are you?");
+// let YearNow = 2022;
+// let UserBorn = YearNow - UserOld;
+// console.log("User was born in",UserBorn,"Year");
+// ------------------------------------------------------------
+// let SumUser = 1298;
+// let Sumbasket = 358;
+// let discount = 0.15;
+// let Delivery = 20;
+// let TotalDiscount = Sumbasket * discount;
+// let TotalUserPay = Sumbasket + Delivery - TotalDiscount ;
+// let SumAfterBuy = SumUser - TotalUserPay;
+// console.log("Your Balance:",SumAfterBuy);
+
+
+// ----------------------------Variant2----------------------
+// let SumUser = 1298;
+// let Sumbasket = 358;
+// let discount = 0.15;
+// let Delivery = 20;
+// let TotalUserPay = (Sumbasket + Delivery) - (Sumbasket * discount) ;
+// let SumAfterBuy = SumUser - TotalUserPay;
+// console.log("Your Balance:",SumAfterBuy);
+
+// ---------------------------true/false text -------------------
+
+// let ask = prompt ("Are you from uk or ru?");
+// let uk = "uk";
+// let ru = " ";
+// let brother = ask == uk;
+// let terorist = ask == ru;
+// console.log ('uk',brother);
+// console.log ('ru',terorist);
+
+// -------------------------numeric true/false------------
+
+let value1 = 28;
+let value2 = 35;
+let value3= 25;
+console.log(value1>20 && value3>30)
+console.log(value1>20 && value2>30)
+
+
+
+
+
+
+
+
+
+
+
+
